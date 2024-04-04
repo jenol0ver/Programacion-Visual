d095672fb179c7e2c35db6b0642839f50864279f
Intel <Intel@DESKTOP-KVMM6CQ>
2024-04-04 15:17:21 -0300

    ejercicio1 resuleto

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		  
+		int a=3;
+		int b=4;
+		int c=2;
+		int d=8;
+		int e=3;
+		double prom= (double)(a+b+c+d+e)/2;
+		
+		System.out.println(a+", "+b+", "+c+", "+d+", "+e+", ");
+		System.out.println("El promedio es:"+prom);
 	}
 
 }
