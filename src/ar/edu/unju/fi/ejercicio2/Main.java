851cad5e44728fdcbab00b5da9c3461ff6555da0
Intel <Intel@DESKTOP-KVMM6CQ>
2024-04-04 15:50:29 -0300

    punto 2

--- src/ar/edu/unju/fi/ejercicio2/Main.java
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		long telefono=3884563478l;
+		int edad=18;
+		String pais= "Argentina";
+		float altura= 12.5f;
+		double precio=(double) 2400.99;
+		double calculo=(double)Math.cos(0.5);
+		
+		System.out.println("Pais: "+pais);
+		System.out.println("Altura: "+altura);
+		System.out.println("Edad: "+edad);
+		System.out.println("Precio: "+precio);
+		System.out.println("Num Telefono: "+telefono);
+		System.out.println("Calculo del coseno de 0.5: "+calculo);
 		
 		
 		
